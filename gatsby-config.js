39de09574cae134e32a28484ff37d2fd07eca8b3
Eduardo Escoto <me@eduardo.wtf>
2021-08-22 05:04:40 -0400

    fontawesome plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-fontawesome-css",
     {
       resolve: "gatsby-plugin-sass",
       sassOptions: {
